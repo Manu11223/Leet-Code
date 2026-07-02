0dcf18a56896aefdf44306d42115b58e83e3fe86
Manohar_manu <ggudimetlamanohar666@gmail.com>
2026-07-02 16:20:54 +0530

    Time: 47 ms (97.36%), Space: 19.2 MB (69.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,24 +2,24 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if not s:
             return ""
-
-        start, end = 0, 0  # indices of the best palindrome found so far
-
-        def expand_from_center(left: int, right: int) -> int:
-            # Expands outward while s[left] == s[right], returns length of palindrome
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return right - left - 1  # length (undo the last invalid expansion)
-
-        for i in range(len(s)):
-            len1 = expand_from_center(i, i)       # odd length, center at i
-            len2 = expand_from_center(i, i + 1)   # even length, center between i and i+1
-            max_len = max(len1, len2)
-
-            if max_len > end - start + 1:
-                # Recompute start/end from center i and max_len
-                start = i - (max_len - 1) // 2
-                end = i + max_len // 2
-
-        return s[start:end + 1]
+        
+        # Transform string: "aba" -> "^#a#b#a#$"
+        t = '#' + '#'.join(s) + '#'
+        n = len(t)
+        p = [0] * n
+        center = right = 0
+        
+        for i in range(n):
+            if i < right:
+                p[i] = min(right - i, p[2 * center - i])
+            
+            # Expand around center i
+            while i - p[i] - 1 >= 0 and i + p[i] + 1 < n and t[i - p[i] - 1] == t[i + p[i] + 1]:
+                p[i] += 1
+            
+            if i + p[i] > right:
+                center, right = i, i + p[i]
+        
+        max_len, center_index = max((n, i) for i, n in enumerate(p))
+        start = (center_index - max_len) // 2
+        return s[start:start + max_len]
