915abba882b2ec8b5accfdde89fa9e7bb2f67b3d
Manohar_manu <ggudimetlamanohar666@gmail.com>
2026-08-07 09:54:29 +0530

    Time: 57 ms (30.25%), Space: 22 MB (99.46%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        
+        # Phase 1: detect if a cycle exists
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None  # fast reached the end, no cycle
+        
+        # Phase 2: find the start of the cycle
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        
+        return slow
