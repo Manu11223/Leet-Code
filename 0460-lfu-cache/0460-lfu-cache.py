6b31f5b1104122b87b6fb60db54acba15d8249fb
Manohar Gudimetla <ggudimetlamanohar666@gmail.com>
2026-09-26 14:23:33 +0530

    Time: 107 ms (95.94%), Space: 79.2 MB (13.47%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -0,0 +1,49 @@
+from collections import OrderedDict, defaultdict
+
+class LFUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.min_freq = 0
+        self.key_to_val_freq = {}  # key -> [value, freq]
+        self.freq_to_keys = defaultdict(OrderedDict)  # freq -> OrderedDict(key -> None)
+
+    def _bump_freq(self, key: int) -> None:
+        value, freq = self.key_to_val_freq[key]
+        del self.freq_to_keys[freq][key]
+        if not self.freq_to_keys[freq]:
+            del self.freq_to_keys[freq]
+            if self.min_freq == freq:
+                self.min_freq += 1
+        
+        new_freq = freq + 1
+        self.key_to_val_freq[key] = [value, new_freq]
+        self.freq_to_keys[new_freq][key] = None
+
+    def get(self, key: int) -> int:
+        if key not in self.key_to_val_freq:
+            return -1
+        self._bump_freq(key)
+        return self.key_to_val_freq[key][0]
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity <= 0:
+            return
+        
+        if key in self.key_to_val_freq:
+            self.key_to_val_freq[key][0] = value
+            self._bump_freq(key)
+            return
+        
+        if len(self.key_to_val_freq) >= self.capacity:
+            evict_key, _ = self.freq_to_keys[self.min_freq].popitem(last=False)
+            del self.key_to_val_freq[evict_key]
+        
+        self.key_to_val_freq[key] = [value, 1]
+        self.freq_to_keys[1][key] = None
+        self.min_freq = 1
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
